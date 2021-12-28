9f22a91673dfbc83bd9b2d3ad8b2ab1f07b9be38
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2021-12-28 18:05:27 -0500

    functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,15 @@
 var now = moment();
 var saved = {};
+
+function init() {
+    if(JSON.parse(localStorage.getItem("scheduleSave") !== null)){
+        saved = JSON.parse(localStorage.getItem("scheduleSave"));
+    };
+
+    for (var i = 0; i < Object.keys(saved).length; i++){
+        $("ul[data-time="+Object.keys(saved)[i]+"]").children(".description").val(saved[Object.keys(saved)[i]]);
+    };
+
+    setInterval(function(){
+        
+    }
