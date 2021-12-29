d75d8ce1afc52a8ff87cfa2a075ad1becda994a4
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2021-12-28 19:06:51 -0500

    function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,29 @@ var now = moment();
 var saved = {};
 
 function init() {
-    if(JSON.parse(localStorage.getItem("scheduleSave") !== null)){
-        saved = JSON.parse(localStorage.getItem("scheduleSave"));
-    };
+  if (JSON.parse(localStorage.getItem("scheduleSave") !== null)) {
+    saved = JSON.parse(localStorage.getItem("scheduleSave"));
+  }
 
-    for (var i = 0; i < Object.keys(saved).length; i++){
-        $("ul[data-time="+Object.keys(saved)[i]+"]").children(".description").val(saved[Object.keys(saved)[i]]);
-    };
+  for (var i = 0; i < Object.keys(saved).length; i++) {
+    $("ul[data-time=" + Object.keys(saved)[i] + "]")
+      .children(".description")
+      .val(saved[Object.keys(saved)[i]]);
+  }
 
-    setInterval(function(){
-        
-    }
+  setInterval(function () {
+    now = moment();
+    $("#currentDay").text(now.format("MMMM Do YYYY, h:mm:ss a"));
+    $("ul").each(function () {
+      if (now.format("H") == $(this).data("time")) {
+        $(this).children(".description").addClass("present");
+      }
+      if (now.format("H") > $(this).data("time")) {
+        $(this).children(".description").addClass("past");
+      }
+      if (now.format("H") < $(this).data("time")) {
+        $(this).children(".description").addClass("future");
+      }
+    });
+  }, 500);
+}
