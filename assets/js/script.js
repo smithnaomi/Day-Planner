1eabed05235bd9b4e78e91df6055338f39ceebf6
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2021-12-28 19:34:38 -0500

    per hour,functions, if statments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,88 @@
-var now = moment();
-var saved = {};
+window.onload = function () {
+  colorCode();
+  nineAM();
+  tenAM();
+  elevenAM();
+  twelvePM();
+  onePM();
+  twoPM();
+  threePM();
+  fourPM();
+  fivePM();
+};
 
-function init() {
-  if (JSON.parse(localStorage.getItem("scheduleSave") !== null)) {
-    saved = JSON.parse(localStorage.getItem("scheduleSave"));
+//CURRENT DATE AND TIME//
+var currentDay = moment().format("dddd LL");
+$("#currentDay").append(currentDay);
+
+var currentTime = moment().format("LT");
+$("#currentTime").append(currentTime);
+
+//HOURS//
+var now = new Date().getHours();
+// BLOCK COLOR CHANGE ON THE HOUR//
+function colorCode() {
+  if (now > 9) {
+    $("#comment9am").addClass("past");
+  } else if (now >= 9 && now < 10) {
+    $("#comment9am").addClass("present");
+  } else if (now < 9) {
+    $("#comment9am").addClass("future");
   }
-
-  for (var i = 0; i < Object.keys(saved).length; i++) {
-    $("ul[data-time=" + Object.keys(saved)[i] + "]")
-      .children(".description")
-      .val(saved[Object.keys(saved)[i]]);
+  if (now > 10) {
+    $("#comment10am").addClass("past");
+  } else if (now >= 10 && now < 11) {
+    $("#comment10am").addClass("present");
+  } else if (now < 10) {
+    $("#comment10am").addClass("future");
   }
-
-  setInterval(function () {
-    now = moment();
-    $("#currentDay").text(now.format("MMMM Do YYYY, h:mm:ss a"));
-    $("ul").each(function () {
-      if (now.format("H") == $(this).data("time")) {
-        $(this).children(".description").addClass("present");
-      }
-      if (now.format("H") > $(this).data("time")) {
-        $(this).children(".description").addClass("past");
-      }
-      if (now.format("H") < $(this).data("time")) {
-        $(this).children(".description").addClass("future");
-      }
-    });
-  }, 500);
+  if (now > 11) {
+    $("#comment11am").addClass("past");
+  } else if (now >= 11 && now < 12) {
+    $("#comment11am").addClass("present");
+  } else if (now < 11) {
+    $("#comment11am").addClass("future");
+  }
+  if (now > 12) {
+    $("#comment12pm").addClass("past");
+  } else if (now >= 12 && now < 13) {
+    $("#comment12pm").addClass("present");
+  } else if (now < 12) {
+    $("#comment12pm").addClass("future");
+  }
+  if (now > 13) {
+    $("#comment1pm").addClass("past");
+  } else if (now >= 13 && now < 14) {
+    $("#comment1pm").addClass("present");
+  } else if (now < 13) {
+    $("#comment1pm").addClass("future");
+  }
+  if (now > 14) {
+    $("#comment2pm").addClass("past");
+  } else if (now >= 14 && now < 15) {
+    $("#comment2pm").addClass("present");
+  } else if (now < 14) {
+    $("#comment2pm").addClass("future");
+  }
+  if (now > 15) {
+    $("#comment3pm").addClass("past");
+  } else if (now >= 15 && now < 16) {
+    $("#comment3pm").addClass("present");
+  } else if (now < 15) {
+    $("#comment3pm").addClass("future");
+  }
+  if (now > 16) {
+    $("#comment4pm").addClass("past");
+  } else if (now >= 16 && now < 17) {
+    $("#comment4pm").addClass("present");
+  } else if (now < 16) {
+    $("#comment4pm").addClass("future");
+  }
+  if (now > 17) {
+    $("#comment5pm").addClass("past");
+  } else if (now >= 17 && now < 18) {
+    $("#comment5pm").addClass("present");
+  } else if (now < 17) {
+    $("#comment5pm").addClass("future");
+  }
 }
