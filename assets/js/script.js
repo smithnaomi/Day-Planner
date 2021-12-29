5eb7c66a48755afb8e1b5d8f039ca6da727217ef
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2021-12-28 20:29:26 -0500

    decreased hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,6 @@ window.onload = function () {
   threePM();
   fourPM();
   fivePM();
-  sixPM();
-  sevenPM();
-
 };
 
 //CURRENT DATE AND TIME//
@@ -80,20 +77,6 @@ function colorCode() {
     $("#comment4pm").addClass("present");
   } else if (now < 16) {
     $("#comment4pm").addClass("future");
-  }
-  if (now > 17) {
-    $("#comment5pm").addClass("past");
-  } else if (now >= 17 && now < 18) {
-    $("#comment5pm").addClass("present");
-  } else if (now < 17) {
-    $("#comment5pm").addClass("future");
-  }
-  if (now > 17) {
-    $("#comment5pm").addClass("past");
-  } else if (now >= 17 && now < 18) {
-    $("#comment5pm").addClass("present");
-  } else if (now < 17) {
-    $("#comment5pm").addClass("future");
   }
   if (now > 17) {
     $("#comment5pm").addClass("past");
